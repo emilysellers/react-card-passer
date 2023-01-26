db6cdb58f8c9661e5b21bc3be8f324c8b6e11793
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-25 16:09:55 -0800

    refactor ExecutePassButton

--- src/components/ExecutePassButton.js
+++ src/components/ExecutePassButton.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useGameContext } from '../context/GameContext.js';
 import Card from './Card';
 
-export default function ExecutePassButton({
-  passCard,
-  setFrom,
-  from,
-  to,
-  selectedCard,
-  setSelectedCard,
-}) {
+// export default function ExecutePassButton({
+//   passCard,
+//   setFrom,
+//   from,
+//   to,
+//   selectedCard,
+//   setSelectedCard,
+export default function ExecutePassButton({ passCard }) {
+  const { selectedCard, from, to, setSelectedCard, setFrom } = useGameContext();
   return (
     <div className="execute-button" onClick={() => passCard(selectedCard)}>
       Pass <Card card={selectedCard} setSelectedCard={setSelectedCard} setFrom={setFrom} /> from{' '}
